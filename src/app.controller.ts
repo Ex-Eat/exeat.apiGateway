e4b626630d02354e28469f1110aba5900de94d83
Hugo De Grossi <personal@hugo-degrossi.fr>
2022-06-22 16:25:34 +0200

    fix controllers and service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,10 +2,10 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+	constructor(private readonly _service: AppService) {}
 
 	@Get('alive')
 	isAlive(): string {
-		return this.appService.isAlive();
+		return this._service.isAlive();
 	}
 }
--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -3,11 +3,11 @@ import { ClientService } from './client.service';
 
 @Controller('client')
 export class ClientController {
-	constructor(private readonly clientService: ClientService) {}
+	constructor(private readonly _service: ClientService) {}
 
 	@Get('alive')
 	async getAlive() {
 		// We should check if the user is connected
-		return this.clientService.getAlive();
+		return this._service.getAlive();
 	}
 }
